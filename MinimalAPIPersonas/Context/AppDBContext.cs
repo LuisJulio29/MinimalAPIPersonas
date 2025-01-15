85f4fcf4c84d2e4fa63231805dca7a2a82d7353f
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-15 10:27:01 -0500

    Agregar archivos de proyecto.

---
+++ MinimalAPIPersonas/Context/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPersonas.Model;
+
+namespace MinimalAPIPersonas.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
+ 
---
+++ MinimalAPIPersonas/Model/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPIPersonas.Model
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int Age { get; set; }
+        public string? Email { get; set; }
+    }
+}
